6b3ac8e1a52db30ec7d744df3755e1038a91a09b
BraenAndDany <braenanddany@mail.ru>
2024-02-13 15:54:32 +0700

    Добавьте файлы проекта.

---
+++ RulCool/Pages/Autho.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RulCool.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для Autho.xaml
+    /// </summary>
+    public partial class Autho : Page
+    {
+        public Autho()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEnterGuest_click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Client());
+        }
+
+        private void btnEnter_click(object sender, RoutedEventArgs e)
+        {
+            string login = txtLogin.Text;
+            string passw = pswbPassword.Password;
+            var dbConn = Entin
+        }
+    }
+
+}
